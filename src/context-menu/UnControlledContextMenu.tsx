5773aa12a80b872d1454eeee8acfdf8d590e9d6e
Armin Jazi <a.ghajarjazi@reply.de>
2024-06-17 09:56:25 +0200

    remove unused import

---
+++ src/context-menu/UnControlledContextMenu.tsx
@@ -0,0 +1,36 @@
+import { useContextMenu } from "~/context-menu/useContextMenu.ts";
+import { ContextMenu } from "~/context-menu/ContextMenu.tsx";
+import { PropsWithChildren, useEffect } from "react";
+
+interface Prop {
+  triggerElement: HTMLElement;
+}
+
+export const UnControlledContextMenu = ({ children, triggerElement }: PropsWithChildren<Prop>) => {
+  const { open, hide, set } = useContextMenu();
+
+  useEffect(() => {
+    const handlePointerDown = (event: PointerEvent) =>{
+      set({
+        x: event.pageX,
+        y: event.pageY,
+        triggerElement,
+      });
+    }
+    triggerElement.addEventListener("pointerdown", handlePointerDown)
+
+    return () => {
+      triggerElement.removeEventListener("pointerdown", handlePointerDown)
+    }
+  }, []);
+
+  return (
+    <ContextMenu
+      open={open}
+      onOutsideClick={hide}
+      triggerElement={triggerElement}
+    >
+      {children}
+    </ContextMenu>
+  );
+};
--- src/tests/QueryWrapper.tsx
+++ src/tests/QueryWrapper.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-import { ToolContextProvider } from "~/tool/ToolContextProvider.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
